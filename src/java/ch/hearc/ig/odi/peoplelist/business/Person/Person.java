aae8f7a906309b6e5146356ba5d95b3a222c8584
unknown <simone.bissolot@EGE-BISSOLOTTIS.intra.eiaj.ch>
2016-10-20 09:03:21 +0200

    création classe person

---
+++ src/java/ch/hearc/ig/odi/peoplelist/business/Person/Person.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.business.Person;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Simone Bissolotti
+ */
+public class Person {
+    private Long id;
+    private String gender;
+    private String firstName;
+    private String lastName;
+    private boolean married;
+    private Date birthDate;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+}
