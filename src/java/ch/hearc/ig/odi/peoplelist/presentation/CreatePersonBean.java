a0c5f508fdac421041612b62e91ad77b41c772da
simone.bissolot <simone.bissolot@EGE-BISSOLOTTIS.intra.eiaj.ch>
2016-10-27 07:59:12 +0200

    internationalisation de la page insertion

---
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/CreatePersonBean.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.presentation;
+
+import ch.hearc.ig.odi.peoplelist.service.Services;
+import java.util.Date;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Simone Bissolotti
+ */
+@Named(value = "createPersonBean")
+@RequestScoped
+public class CreatePersonBean {
+
+     @Inject Services services;
+    /**
+     * Creates a new instance of CreatePersonBean
+     */
+    public CreatePersonBean() {
+    }
+    
+    private Long id;
+    private String gender;
+    private String firstName;
+    private String lastName;
+    private boolean married;
+    private Date birthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+            this.birthDate = birthDate;
+    }
+    
+     public Long createPerson(){
+       return services.savePerson(gender, firstName, lastName, married, birthDate);
+    }
+    
+}
--- src/java/ch/hearc/ig/odi/peoplelist/presentation/PeopleListBean.java
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/PeopleListBean.java
@@ -26,61 +26,6 @@ public class PeopleListBean {
 
     @Inject Services services;
     
-    private Long id;
-    private String gender;
-    private String firstName;
-    private String lastName;
-    private boolean married;
-    private Date birthDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public boolean isMarried() {
-        return married;
-    }
-
-    public void setMarried(boolean married) {
-        this.married = married;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-            this.birthDate = birthDate;
-    }
-    
     
     /**
      * Creates a new instance of PeopleListBean
@@ -92,7 +37,4 @@ public class PeopleListBean {
         return services.getPeopleList();
     }
     
-    public Long createPerson(){
-       return services.savePerson(gender, firstName, lastName, married, birthDate);
-    }
 }
